f56c90767c610d9c4a3b3b55a203ead79fbca389
Tomer Ezra <45719201+tomerezra@users.noreply.github.com>
2019-04-03 17:04:28 +0300

    tralalala

--- Desktop/Desktop/App_Start/WebApiConfig.cs
+++ Desktop/Desktop/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Desktop
 {
@@ -12,6 +13,8 @@ namespace Desktop
             // Web API configuration and services
 
             // Web API routes
+            var corsAttr = new EnableCorsAttribute("http://localhost:3000", "*", "*");
+            config.EnableCors(corsAttr);
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
